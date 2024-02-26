cb6b5a49fa6b6a6683a3386fd380128193b33533
Seetha <seetha@resemblesystems.com>
2024-02-26 14:31:54 +0530

    new changes

---
+++ src/webparts/printWorkPermit/components/IPrintWorkPermitProps.ts
@@ -0,0 +1,7 @@
+export interface IPrintWorkPermitProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/ssimsDashboard/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISsimsDashboardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SsimsDashboardWebPartStrings' {
+  const strings: ISsimsDashboardWebPartStrings;
+  export = strings;
+}
